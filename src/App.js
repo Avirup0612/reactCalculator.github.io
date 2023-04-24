ce6d3a0482f60982522e1683a37f52f69fe9b8bc
Avirup0612 <mitraavirup1@gmail.com>
2023-04-24 14:28:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './style.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [result, setResult] = useState();
+  const[doMath, setMath] = useState("");
+
+  function handleClick(e){
+    setMath(doMath.concat(e.target.value));
+  }
+  
+  function allClear(e){
+    setMath("");
+    setResult("");
+  }
+
+  function deleteOne(e){
+    setMath(doMath.slice(0,-1));
+  }
+
+  function calcPercentage() {
+    const resultPercent=eval(doMath/100);
+    setMath(resultPercent.toString().concat("*"));
+  }
+  
+  function showResult() {
+    try{
+      setResult(eval(doMath));
+    }
+    catch(error) {
+      setResult("Error");
+    }
+
+  }
+
+  return(
+    <>
+      <div className="calculator">
+      <h2>Calculator</h2>
+        <div className="display1">
+          <textarea  type="text" className="math" name="math" readOnly value={doMath}/>
+        </div>
+        <div className="display2">
+          <input type="text" className="result" name="result" readOnly value={result}/>
+        </div>
+        <div className='firstrow'>
+          <input type="button" name="clear" value="AC" onClick={allClear}/>
+          <input type="button" name="delete" value="D" onClick={deleteOne}/>
+          <input type="button" name="percentage" value="%" onClick={calcPercentage}/>
+          <input type="button" name="division" value="/" onClick={handleClick}/>
+        </div>
+        <div className='secondrow'>
+          <input type="button" name="7" value="7" onClick={handleClick}/>
+          <input type="button" name="8" value="8" onClick={handleClick}/>
+          <input type="button" name="9" value="9" onClick={handleClick}/>
+          <input type="button" name="multiplication" value="*" onClick={handleClick}/>
+        </div>
+        <div className='thirdrow'>
+          <input type="button" name="4" value="4" onClick={handleClick}/>
+          <input type="button" name="5" value="5" onClick={handleClick}/>
+          <input type="button" name="6" value="6" onClick={handleClick}/>
+          <input type="button" name="addition" value="+" onClick={handleClick}/>
+        </div>
+        <div className='fourthrow'>
+          <input type="button" name="1" value="1" onClick={handleClick}/>
+          <input type="button" name="2" value="2" onClick={handleClick}/>
+          <input type="button" name="3" value="3" onClick={handleClick}/>
+          <input type="button" name="substraction" value="-" onClick={handleClick}/>
+        </div>
+        <div className='fifthrow'>
+          <input type="button" name="0" value="0" onClick={handleClick}/>
+          <input type="button" name="." value="." onClick={handleClick}/>
+          <input type="button" name="equalto" value="=" onClick={showResult}/>
+        </div>
+      </div>
+    </>
+  )
 }
+
 
 export default App;
